b9f981666deef532ec391d2db91d0c51de8273d9
dkaaa01 <dmb08230@naver.com>
2022-11-18 21:49:33 +0900

    Initial Commit

---
+++ connect_gitHub/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  connect_gitHub
+//
+//  Created by 김성원 on 2022/11/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
